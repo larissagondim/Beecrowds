64dd017f2d21c796f17b276f82d8a2723675ef2e
larissagondim <gondimvilasboaslarissa@gmail.com>
2026-02-26 11:13:09 -0300

    feat: new math problems

---
+++ C++/Mathematics/1585.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    int a, b;
+    while(t--) {
+	cin >> a >> b; cout << (a*b)/2 << " cm2" <<  endl;
+    }
+    return 0;
+}
